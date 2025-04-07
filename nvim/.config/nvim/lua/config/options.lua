45ae856e08bda8260a99d019abeac13e9574599e
Kishan V <kishan-v@users.noreply.github.com>
2025-04-07 18:07:55 +0100

    Alternative statuscolumn line number configuration
    Not perfect (see code comments)
    
    Introduced due to breaking statuscolumn change in neovim v0.110.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,9 +3,12 @@ local opt = vim.opt
 vim.opt.clipboard = "unnamedplus"
 
 opt.number = true
-vim.opt.numberwidth = 5 -- Sets the minimal width for the number column
+-- vim.opt.numberwidth = 5 -- Sets the minimal width for the number column
 opt.relativenumber = true
-vim.opt.statuscolumn = "%s %l %r  " -- Show absolute line numbers (for all lines) in addition to relative line number
+-- vim.opt.statuscolumn = "%s %l %r  " -- Show absolute line numbers (for all lines) in addition to relative line number. NOTE: THIS WAS BROKEN IN NeoVim v0.11.0
+-- ATTEMPTED FIXES:
+vim.o.statuscolumn = [[%! (v:virtnum == 0 ? printf('%3d%3d   ', v:lnum, abs(v:lnum - line('.'))) : '         ') ]] -- Show absolute line numbers (for all lines) in addition to relative line number. Note: may not handle signs correctly. Note: the padding between absolute and relative line numbers may disappear when relative line numbers show 3 digits or more.
+-- vim.opt.statuscolumn = "%s %{v:virtnum==0?v:lnum:''} %{v:virtnum==0?v:relnum:''} "  -- Shows signs correctly but padding is weird
 
 opt.termguicolors = true
 
