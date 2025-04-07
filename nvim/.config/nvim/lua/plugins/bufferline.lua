321da93078a102268a94587ff1e4281b0d47e3c7
Kishan V <kishan-v@users.noreply.github.com>
2025-04-07 15:10:26 +0100

    Updating nvim-config 07/02/25
    Updating plugin versions, adding copilotchat
    
    Moving README contents to repo root

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -5,6 +5,7 @@ return {
 		dependencies = "nvim-tree/nvim-web-devicons",
 		opts = {
 			options = {
+				-- TODO: Add padding to account for nvim-tree file explorer
 				diagnostic = "nvim-lsp", -- FIXME: Add this to the correct place
 				hover = {
 					enabled = true,
